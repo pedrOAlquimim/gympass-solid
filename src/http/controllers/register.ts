ebd9968da61f13ac91a56fbe1b2850fce47a117c
Pedro Alquimim <pedrohalquimim@gmail.com>
2023-08-14 21:17:11 -0300

    feat: hash for passwords

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@/lib/prisma"
 import { z } from "zod"
+import { hash } from 'bcryptjs'
 import { FastifyReply, FastifyRequest } from "fastify"
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -11,11 +12,23 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = requestBodySchema.parse(request.body)
 
+  const password_hash = await hash(password, 6)
+
+  const userEmailExist = await prisma.user.findUnique({
+    where: {
+      email,
+    }
+  })
+
+  if (userEmailExist) {
+    return reply.status(409).send()
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password
+      password_hash,
     }
   })
 
