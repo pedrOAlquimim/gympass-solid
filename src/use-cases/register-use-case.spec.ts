390fbea373dcf088e56a76c677641d329412ef80
Pedro Alquimim <pedrohalquimim@gmail.com>
2023-08-19 14:56:21 -0300

    test: create unit test to test if hasg password upon registration

--- src/use-cases/register-use-case.spec.ts
+++ src/use-cases/register-use-case.spec.ts
@@ -1,8 +1,36 @@
 import { describe, it, expect } from 'vitest'
+import { RegisterUseCase } from './register-use-case'
+import { compare } from 'bcryptjs'
 
-describe('vitest its working', () => {
-  it('should be able to sum 2 + 2', () => {
+describe('Register use case', () => {
+  it('should hash password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date()
+        }
+      },
 
-    expect(2 + 2).toBe(4)
+      async findByEmail(email) {
+        return null
+      },
+    })
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+    
+    const isPasswordCorretlyHashed = await compare(
+      '123456',
+      user.password_hash
+    )
+
+    expect(isPasswordCorretlyHashed).toBe(true)
   })
 })
