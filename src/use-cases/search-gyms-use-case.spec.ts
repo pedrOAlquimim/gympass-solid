90dc140a98014986807e27e2447922f8e3f83d5f
Pedro Alquimim <pedrohalquimim@gmail.com>
2023-08-28 23:58:06 -0300

    feat: search gym paginated use case

---
+++ src/use-cases/search-gyms-use-case.spec.ts
@@ -0,0 +1,56 @@
+import { InMemoryCheckInRepository } from "@/repositories/in-memory/in-memory-check-in-repository";
+import { beforeEach, describe, expect, it } from "vitest";
+import { FecthUserCheckInsHistoryUseCase } from "./fetch-user-check-ins-history-use-case";
+import { InMemoryGymRepository } from "@/repositories/in-memory/in-memory-gym-repository";
+import { SearchGymUseCase } from "./search-gyms-use-case";
+import { Prisma } from "@prisma/client";
+
+let inMemoryGymsRepository: InMemoryGymRepository
+let sut: SearchGymUseCase
+
+describe('Search Gyms use Case', () => {
+  beforeEach(async () => {
+    inMemoryGymsRepository = new InMemoryGymRepository()
+    sut = new SearchGymUseCase(inMemoryGymsRepository)
+  })
+  
+  it('should be able get a gym', async () => {
+    await inMemoryGymsRepository.create({
+      description: '',
+      title: 'JS Academy',
+      phone: '',
+      latitude: new Prisma.Decimal(-22.9305763),
+      longitude: new Prisma.Decimal(-47.0312354), 
+    })
+
+    const { gyms } = await sut.execute({
+      query: 'JS',
+      page: 1,
+    })
+
+    expect(gyms).toEqual([expect.objectContaining({ title: 'JS Academy' })])
+  })
+
+  it('should be able to fetch paginated gyms', async () => {
+    for(let i = 1; i <= 22; i++) {
+      await inMemoryGymsRepository.create({
+        description: '',
+        title: `JS Academy ${i}`,
+        phone: '',
+        latitude: new Prisma.Decimal(-22.9305763),
+        longitude: new Prisma.Decimal(-47.0312354), 
+      })
+    }
+
+    const { gyms } = await sut.execute({
+      query: 'JS',
+      page: 2,
+    })
+
+    expect(gyms).toHaveLength(2)
+    expect(gyms).toEqual([
+      expect.objectContaining({title: 'JS Academy 21'}),
+      expect.objectContaining({title: 'JS Academy 22'})
+    ])
+  })
+})
