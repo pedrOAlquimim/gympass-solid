03c78c901d00307dc0d18e1d272c218cf5d7407d
Pedro Alquimim <pedrohalquimim@gmail.com>
2023-09-03 19:53:53 -0300

    feat: create profile controller and verifyJwt middleware

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,25 @@
+import { ResourceNotFoundError } from "@/use-cases/errors/resource-not-found-error";
+import { PrismaGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
+  try {
+    const getUserProfileUseCase = new PrismaGetUserProfileUseCase()
 
-  console.log(request.user.sub)
+    const { user } = await getUserProfileUseCase.factoryMethod().execute({
+      userId: request.user.sub
+    })
 
-  reply.status(200).send()
+    return reply.status(200).send({
+      ...user,
+      password_hash: undefined     
+    })
+  }
+  catch (error) {
+    if (error instanceof ResourceNotFoundError) {
+      return reply.status(400).send({message: error.message})
+    }
+
+    throw error
+  }
 }
