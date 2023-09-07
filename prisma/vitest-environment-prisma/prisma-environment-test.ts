2232694f7f0ec6e7d715cc05cb7d7ec6e6ff5166
Pedro Alquimim <pedrohalquimim@gmail.com>
2023-09-07 00:20:22 -0300

    feat: create prisma test environment to a new database

--- prisma/vitest-environment-prisma/prisma-environment-test.ts
+++ prisma/vitest-environment-prisma/prisma-environment-test.ts
@@ -1,14 +1,41 @@
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
 import type { Environment } from 'vitest'
+import { execSync } from 'node:child_process'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDataBaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please use a true db url!')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.href.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
-  transformMode: 'web',
+  transformMode: 'ssr',
   async setup() {
-    console.log('setup')
+    const schema = randomUUID()
+    const dataBaseUrl = generateDataBaseUrl(schema)
+
+    process.env.DATABASE_URL = dataBaseUrl
+
+    execSync('npx prisma migrate deploy')
 
     return {
       async teardown() {
-        console.log('teardown')
+        await prisma.$executeRawUnsafe(`
+          DROP SCHEMA IF EXISTS "${schema}" CASCADE
+        `)
+
+        await prisma.$disconnect()
       }
     }
   }
