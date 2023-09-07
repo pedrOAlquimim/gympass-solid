bda2cd16b8ab51fccb7256ac890cea86c64cbcc3
Pedro Alquimim <pedrohalquimim@gmail.com>
2023-09-07 00:57:38 -0300

    test: create test for register controller

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,25 @@
-import { expect, test } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import supertest from 'supertest'
+import { app } from "@/app";
 
-test('sum 2 + 2 should be 4', () => {
-  expect(2 + 2).toBe(4)
+beforeAll(async () => {
+  await app.ready()
+})
+
+afterAll(async () => {
+  await app.close()
+})
+
+describe('Register controller`(E2E)', () => {
+  it('should be able to register a new user', async () => {
+    const register = await supertest(app.server)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'johnsdoe@example.com',
+        password: '123456',
+      })
+    
+      expect(register.statusCode).toEqual(201)
+  })
 })
